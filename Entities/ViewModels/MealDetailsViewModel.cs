8eea0448510d11b2cd030cc6c353a22b464c18d1
Dogukan Dogus <dogukandogus98@gmail.com>
2023-02-15 12:19:02 +0300

    ViewModels eklemesi

---
+++ Entities/ViewModels/MealDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities.ViewModels
+{
+    public class MealDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string MealType { get; set; }
+        public string Food { get; set; }
+        public double Gram { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
