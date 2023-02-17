914653fad4aa250ddbbefa913831ab3b305314b0
Dogukan Dogus <dogukandogus98@gmail.com>
2023-02-17 17:49:19 +0300

    frmReports eklendi

--- CalorieTracker/frmReports.Designer.cs
+++ CalorieTracker/frmReports.Designer.cs
@@ -40,6 +40,20 @@
             this.btnHome = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panelMenu = new System.Windows.Forms.Panel();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.btnReport = new System.Windows.Forms.Button();
+            this.lsvFoods = new System.Windows.Forms.ListView();
+            this.clmFoodName = new System.Windows.Forms.ColumnHeader();
+            this.clmUnitCalorie = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sqlCommand1 = new Microsoft.Data.SqlClient.SqlCommand();
+            this.lsvMealTypes = new System.Windows.Forms.ListView();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDeleteFood = new System.Windows.Forms.Button();
+            this.btnDeleteMeal = new System.Windows.Forms.Button();
             this.panelTitleBar.SuspendLayout();
             this.panelMenu.SuspendLayout();
             this.SuspendLayout();
@@ -48,10 +62,10 @@
             // 
             this.panelBottom.BackColor = System.Drawing.Color.OliveDrab;
             this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelBottom.Location = new System.Drawing.Point(0, 504);
-            this.panelBottom.Margin = new System.Windows.Forms.Padding(2);
+            this.panelBottom.Location = new System.Drawing.Point(0, 672);
+            this.panelBottom.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.panelBottom.Name = "panelBottom";
-            this.panelBottom.Size = new System.Drawing.Size(360, 16);
+            this.panelBottom.Size = new System.Drawing.Size(411, 21);
             this.panelBottom.TabIndex = 7;
             // 
             // btnCloseApp
@@ -62,10 +76,10 @@
             this.btnCloseApp.FlatAppearance.MouseDownBackColor = System.Drawing.Color.YellowGreen;
             this.btnCloseApp.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OliveDrab;
             this.btnCloseApp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCloseApp.Location = new System.Drawing.Point(318, 10);
-            this.btnCloseApp.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCloseApp.Location = new System.Drawing.Point(363, 13);
+            this.btnCloseApp.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.btnCloseApp.Name = "btnCloseApp";
-            this.btnCloseApp.Size = new System.Drawing.Size(28, 24);
+            this.btnCloseApp.Size = new System.Drawing.Size(32, 32);
             this.btnCloseApp.TabIndex = 10;
             this.btnCloseApp.Text = "X";
             this.btnCloseApp.UseVisualStyleBackColor = false;
@@ -79,10 +93,10 @@
             this.btnMinimizeApp.FlatAppearance.MouseDownBackColor = System.Drawing.Color.YellowGreen;
             this.btnMinimizeApp.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OliveDrab;
             this.btnMinimizeApp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMinimizeApp.Location = new System.Drawing.Point(280, 10);
-            this.btnMinimizeApp.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMinimizeApp.Location = new System.Drawing.Point(320, 13);
+            this.btnMinimizeApp.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.btnMinimizeApp.Name = "btnMinimizeApp";
-            this.btnMinimizeApp.Size = new System.Drawing.Size(28, 24);
+            this.btnMinimizeApp.Size = new System.Drawing.Size(32, 32);
             this.btnMinimizeApp.TabIndex = 4;
             this.btnMinimizeApp.Text = "_";
             this.btnMinimizeApp.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -95,10 +109,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe Print", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(123, 3);
+            this.label1.Location = new System.Drawing.Point(141, 4);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 35);
+            this.label1.Size = new System.Drawing.Size(143, 44);
             this.label1.TabIndex = 3;
             this.label1.Text = "REPORTS";
             // 
@@ -109,9 +123,9 @@
             this.panelTitleBar.Controls.Add(this.label1);
             this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelTitleBar.Location = new System.Drawing.Point(0, 0);
-            this.panelTitleBar.Margin = new System.Windows.Forms.Padding(2);
+            this.panelTitleBar.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.panelTitleBar.Name = "panelTitleBar";
-            this.panelTitleBar.Size = new System.Drawing.Size(360, 40);
+            this.panelTitleBar.Size = new System.Drawing.Size(411, 53);
             this.panelTitleBar.TabIndex = 9;
             this.panelTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitleBar_MouseDown);
             // 
@@ -124,10 +138,10 @@
             this.btnUser.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.btnUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnUser.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnUser.Location = new System.Drawing.Point(255, 0);
-            this.btnUser.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUser.Location = new System.Drawing.Point(292, 0);
+            this.btnUser.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.btnUser.Name = "btnUser";
-            this.btnUser.Size = new System.Drawing.Size(75, 40);
+            this.btnUser.Size = new System.Drawing.Size(86, 53);
             this.btnUser.TabIndex = 3;
             this.btnUser.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnUser.UseVisualStyleBackColor = false;
@@ -142,10 +156,10 @@
             this.btnMeal.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.btnMeal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMeal.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnMeal.Location = new System.Drawing.Point(180, 0);
-            this.btnMeal.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMeal.Location = new System.Drawing.Point(206, 0);
+            this.btnMeal.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.btnMeal.Name = "btnMeal";
-            this.btnMeal.Size = new System.Drawing.Size(75, 40);
+            this.btnMeal.Size = new System.Drawing.Size(86, 53);
             this.btnMeal.TabIndex = 2;
             this.btnMeal.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnMeal.UseVisualStyleBackColor = false;
@@ -160,10 +174,10 @@
             this.btnReports.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.btnReports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnReports.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnReports.Location = new System.Drawing.Point(105, 0);
-            this.btnReports.Margin = new System.Windows.Forms.Padding(2);
+            this.btnReports.Location = new System.Drawing.Point(120, 0);
+            this.btnReports.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.btnReports.Name = "btnReports";
-            this.btnReports.Size = new System.Drawing.Size(75, 40);
+            this.btnReports.Size = new System.Drawing.Size(86, 53);
             this.btnReports.TabIndex = 1;
             this.btnReports.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnReports.UseVisualStyleBackColor = false;
@@ -178,10 +192,10 @@
             this.btnHome.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnHome.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnHome.Location = new System.Drawing.Point(30, 0);
-            this.btnHome.Margin = new System.Windows.Forms.Padding(2);
+            this.btnHome.Location = new System.Drawing.Point(34, 0);
+            this.btnHome.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(75, 40);
+            this.btnHome.Size = new System.Drawing.Size(86, 53);
             this.btnHome.TabIndex = 0;
             this.btnHome.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnHome.UseVisualStyleBackColor = false;
@@ -192,9 +206,9 @@
             this.panel3.BackColor = System.Drawing.Color.OliveDrab;
             this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(30, 40);
+            this.panel3.Size = new System.Drawing.Size(34, 53);
             this.panel3.TabIndex = 0;
             // 
             // panelMenu
@@ -206,29 +220,176 @@
             this.panelMenu.Controls.Add(this.btnHome);
             this.panelMenu.Controls.Add(this.panel3);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelMenu.Location = new System.Drawing.Point(0, 464);
-            this.panelMenu.Margin = new System.Windows.Forms.Padding(2);
+            this.panelMenu.Location = new System.Drawing.Point(0, 619);
+            this.panelMenu.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.panelMenu.Name = "panelMenu";
-            this.panelMenu.Size = new System.Drawing.Size(360, 40);
+            this.panelMenu.Size = new System.Drawing.Size(411, 53);
             this.panelMenu.TabIndex = 8;
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.CalendarForeColor = System.Drawing.SystemColors.Window;
+            this.dtpDate.CalendarTitleBackColor = System.Drawing.SystemColors.Window;
+            this.dtpDate.CalendarTitleForeColor = System.Drawing.SystemColors.Window;
+            this.dtpDate.CalendarTrailingForeColor = System.Drawing.SystemColors.Window;
+            this.dtpDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dtpDate.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dtpDate.Location = new System.Drawing.Point(12, 83);
+            this.dtpDate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(210, 30);
+            this.dtpDate.TabIndex = 11;
+            // 
+            // btnReport
+            // 
+            this.btnReport.Location = new System.Drawing.Point(267, 83);
+            this.btnReport.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
+            this.btnReport.Name = "btnReport";
+            this.btnReport.Size = new System.Drawing.Size(111, 30);
+            this.btnReport.TabIndex = 12;
+            this.btnReport.Text = "Report";
+            this.btnReport.UseVisualStyleBackColor = true;
+            // 
+            // lsvFoods
+            // 
+            this.lsvFoods.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lsvFoods.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lsvFoods.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmFoodName,
+            this.clmUnitCalorie,
+            this.columnHeader1});
+            this.lsvFoods.FullRowSelect = true;
+            this.lsvFoods.Location = new System.Drawing.Point(34, 299);
+            this.lsvFoods.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.lsvFoods.Name = "lsvFoods";
+            this.lsvFoods.Size = new System.Drawing.Size(318, 212);
+            this.lsvFoods.TabIndex = 13;
+            this.lsvFoods.UseCompatibleStateImageBehavior = false;
+            this.lsvFoods.View = System.Windows.Forms.View.Details;
+            // 
+            // clmFoodName
+            // 
+            this.clmFoodName.Text = "Food Name";
+            this.clmFoodName.Width = 140;
+            // 
+            // clmUnitCalorie
+            // 
+            this.clmUnitCalorie.Text = "Unit Calorie";
+            this.clmUnitCalorie.Width = 90;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Gram";
+            this.columnHeader1.Width = 90;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(12, 541);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(173, 19);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Total Calories in Meal :";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 586);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 19);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Total Calories in Daily :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sqlCommand1
+            // 
+            this.sqlCommand1.CommandTimeout = 30;
+            this.sqlCommand1.Connection = null;
+            this.sqlCommand1.Notification = null;
+            this.sqlCommand1.Transaction = null;
+            // 
+            // lsvMealTypes
+            // 
+            this.lsvMealTypes.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lsvMealTypes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lsvMealTypes.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2});
+            this.lsvMealTypes.FullRowSelect = true;
+            this.lsvMealTypes.Location = new System.Drawing.Point(34, 144);
+            this.lsvMealTypes.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.lsvMealTypes.Name = "lsvMealTypes";
+            this.lsvMealTypes.Size = new System.Drawing.Size(201, 132);
+            this.lsvMealTypes.TabIndex = 18;
+            this.lsvMealTypes.UseCompatibleStateImageBehavior = false;
+            this.lsvMealTypes.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Meals";
+            this.columnHeader2.Width = 200;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.OliveDrab;
+            this.btnEdit.Location = new System.Drawing.Point(267, 554);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(111, 40);
+            this.btnEdit.TabIndex = 19;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            // 
+            // btnDeleteFood
+            // 
+            this.btnDeleteFood.Location = new System.Drawing.Point(267, 220);
+            this.btnDeleteFood.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
+            this.btnDeleteFood.Name = "btnDeleteFood";
+            this.btnDeleteFood.Size = new System.Drawing.Size(111, 40);
+            this.btnDeleteFood.TabIndex = 20;
+            this.btnDeleteFood.Text = "Delete Food";
+            this.btnDeleteFood.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteMeal
+            // 
+            this.btnDeleteMeal.Location = new System.Drawing.Point(267, 163);
+            this.btnDeleteMeal.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
+            this.btnDeleteMeal.Name = "btnDeleteMeal";
+            this.btnDeleteMeal.Size = new System.Drawing.Size(111, 40);
+            this.btnDeleteMeal.TabIndex = 21;
+            this.btnDeleteMeal.Text = "Delete Meal";
+            this.btnDeleteMeal.UseVisualStyleBackColor = true;
             // 
             // frmReports
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(360, 520);
+            this.ClientSize = new System.Drawing.Size(411, 693);
             this.ControlBox = false;
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDeleteFood);
+            this.Controls.Add(this.btnDeleteMeal);
+            this.Controls.Add(this.lsvMealTypes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lsvFoods);
+            this.Controls.Add(this.btnReport);
+            this.Controls.Add(this.dtpDate);
             this.Controls.Add(this.btnCloseApp);
             this.Controls.Add(this.panelTitleBar);
             this.Controls.Add(this.panelMenu);
             this.Controls.Add(this.panelBottom);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmReports";
             this.Load += new System.EventHandler(this.frmStatistics_Load);
             this.panelTitleBar.ResumeLayout(false);
             this.panelTitleBar.PerformLayout();
             this.panelMenu.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -245,5 +406,19 @@
         private Button btnHome;
         private Panel panel3;
         private Panel panelMenu;
+        private DateTimePicker dtpDate;
+        private Button btnReport;
+        private ListView lsvFoods;
+        private ColumnHeader clmFoodName;
+        private ColumnHeader clmUnitCalorie;
+        private ColumnHeader columnHeader1;
+        private Label label5;
+        private Label label2;
+        private Microsoft.Data.SqlClient.SqlCommand sqlCommand1;
+        private ListView lsvMealTypes;
+        private ColumnHeader columnHeader2;
+        private Button btnEdit;
+        private Button btnDeleteFood;
+        private Button btnDeleteMeal;
     }
 }
